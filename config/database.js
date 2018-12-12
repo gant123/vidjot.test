0e5a3ea9ee1a3d9be15704121e9d25ff70d17506
gant123 <demontegant@gmail.com>
2018-12-12 12:59:33 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-  module.exports = {mongoURI: 'mongodb://gant123:6701221m@ds149373.mlab.com:49373/vidjot-prod'}
+  module.exports = {mongoURI: 'mongodb://gant123:@ds149373.mlab.com:49373/vidjot-prod'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
